8385b9d4f27b535c65ab7fbdf239ea207ed66a70
= <=>
2022-06-09 10:21:26 +0700

    update

--- src/screens/listSoc.js
+++ src/screens/listSoc.js
@@ -4,8 +4,13 @@ import { Text, Box, FlatList, Pressable } from "native-base";
 // Add Props in Hello({navigation})
 export default function Hello({ navigation }) {
   // Set Dummy Data with Array
+  const socialMedia = ["Instagram", "Facebook", "Twitter", "Line"]
 
   // Make Function handle press to get value per list
+  const handlePress = (value) => {
+    const data = "Ini data lain"
+    navigation.navigate("Detail Social", { value, data })
+  }
 
   return (
     <Box
@@ -17,6 +22,21 @@ export default function Hello({ navigation }) {
       p={10}
     >
       {/* Render Array With Flatlist */}
+      <FlatList
+        data={socialMedia}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => handlePress(item)}>
+            <Text
+              fontFamily="body"
+              fontWeight={400}
+              fontStyle="italic"
+              fontSize={30}
+            >
+              {item}
+            </Text>
+          </Pressable>
+        )}
+      />
     </Box>
   );
 }
