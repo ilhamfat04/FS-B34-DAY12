aff3ff00c546074304b6cfdc6151adccb357c96b
= <=>
2022-06-09 11:06:35 +0700

    update

--- src/screens/DetailPost.js
+++ src/screens/DetailPost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -10,25 +10,73 @@ import {
 import { ListItem, Avatar } from "react-native-elements";
 
 // Import Axios
+import axios from "axios";
 
 const PostDetail = (props) => {
   //init Props
+  const { title, body, id } = props.route.params
 
   //Init State
+  const [comments, setComments] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   // Create LifeCycle
+  useEffect(() => {
+    getComment()
+  }, [])
+
   //Function Exception
+  const getComment = () => {
+    setIsLoading(true)
+    axios
+      .get(`https://jsonplaceholder.typicode.com/post/${id}/comments`)
+      .then((res) => {
+        setComments(res.data)
+        setIsLoading(false)
+      })
+      .catch(() => {
+        alert("Error fetch data")
+        setIsLoading(false)
+      })
+  }
+
+  console.log(comments);
 
   // Create Function to fetch
+  const _renderItem = ({ item }) => {
+    return (
+      <ListItem
+        key={item.id.toString}
+        bottomDivider
+      >
+        <ListItem.Content>
+          <ListItem.Title h4 numberOfLines={1}>
+            {item.email}
+          </ListItem.Title>
+          <ListItem.Subtitle numberOfLines={2}>
+            {`${item.name} - ${item.body}`}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    )
+  }
+
 
   //   Create Component List
   return (
     <View style={style.container}>
-      <Text h2 style={{ fontWeight: "bold" }}>
-        This Is Post Detail
+      <Text h1 style={{ fontWeight: "bold" }}>
+        {title}
       </Text>
-      {/* Implement Axios Here */}
-      {/* Render Component List */}
+      <Text>Comment</Text>
+      <FlatList
+        data={comments}
+        renderItem={_renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={getComment} />
+        }
+      />
     </View>
   );
 };
--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,28 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet, FlatList } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
 
 // Import Axios
+import axios from "axios";
 
 const Posts = (props) => {
   //Init State
+  const [post, setPost] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   // Create LifeCycle
-  //Function Exception
+  useEffect(() => {
+    getPost()
+  }, [])
 
+  //Function Exception
+  const getPost = () => {
+    setIsLoading(true)
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        setPost(res.data)
+        setIsLoading(false)
+      })
+      .catch(() => {
+        alert("Error fetch data")
+        setIsLoading(false)
+      })
+  }
+
+  console.log(post);
   // Create Function to fetch
+  const _renderItem = ({ item }) => {
+    return (
+      <ListItem
+        onPress={() => props.navigation.navigate("DetailPost", item)}
+        key={item.id.toString}
+        bottomDivider
+      >
+        <Avatar
+          rounded
+          title={item.title.slice(0, 2)}
+          containerStyle={{ backgroundColor: "black" }}
+        />
+        <ListItem.Content>
+          <ListItem.Title h4 numberOfLines={1}>
+            {item.title}
+          </ListItem.Title>
+          <ListItem.Subtitle numberOfLines={2}>
+            {item.body}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    )
+  }
 
   //   Create Component List
   return (
     <View style={style.container}>
       <View>
-        <Text>This Is Post</Text>
-
-        <Button
-          title="To Detail Post"
-          onPress={() => props.navigation.navigate("DetailPost")}
+        <FlatList
+          data={post}
+          renderItem={_renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          refreshing={isLoading}
+          onRefresh={getPost}
         />
-        {/* Render Component List */}
       </View>
     </View>
   );
